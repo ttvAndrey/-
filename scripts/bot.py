ebe0983ac3325a6e0e2198309a02006f7d7fdaf3
Andrey <163730995+ttvAndrey@users.noreply.github.com>
2024-05-29 21:36:53 +0300

    корзина и  ассортимент

--- scripts/bot.py
+++ scripts/bot.py
@@ -1,8 +1,16 @@
 import telebot
 from telebot import types
 
+price_list  = {'estetik 50': 3500,
+                'estetik 50': 3500,
+                'estetik 50': 3500}
+
+
 class Bot:
     bot = None
+    cart = []
+    
+        
     def __init__(self,token):
         self.bot = telebot.TeleBot(token)
         #main_markup = types.InlineKeyboardMarkup(row_width=2)
@@ -19,18 +27,53 @@ class Bot:
             markup.add(button1,button2,button3,button4,button5,button6)
             #main_markup = markup
             self.bot.send_photo(message.chat.id, photo=open("../images/start.jpg","rb"), caption="Привет, вы попали в магазин по продаже свечей!", reply_markup=markup)
+        @self.bot.message_handler()
+        def estetik(message):#здесь
+            markup = types.InlineKeyboardMarkup(row_width=2)
+            button1 = types.InlineKeyboardButton("Купить",callback_data='estetik 180')
+            markup.add(button1)
+            self.bot.edit_message_media(media=types.InputMediaPhoto(open("../images/menu.jpg",'rb')),chat_id=message.chat.id,message_id=message.message_id)
+            self.bot.edit_message_caption("Купить Estetik Объемом 180 мл, Цена - 3500 ₽",message.chat.id,message.message_id,reply_markup=markup)
+        @self.bot.message_handler()
+        def bionika(message):#здесь
+            markup = types.InlineKeyboardMarkup(row_width=2)
+            button1 = types.InlineKeyboardButton("Купить",callback_data='bionika 160')
+            markup.add(button1)
+            self.bot.edit_message_media(media=types.InputMediaPhoto(open("../images/menu.jpg",'rb')),chat_id=message.chat.id,message_id=message.message_id)
+            self.bot.edit_message_caption("Купить Bionika Объемом 160 мл, Цена - 4200 ₽",message.chat.id,message.message_id,reply_markup=markup)
+        def shick(message):#здесь
+            markup = types.InlineKeyboardMarkup(row_width=2)
+            button1 = types.InlineKeyboardButton("Купить",callback_data='shick 110')
+            markup.add(button1)
+            self.bot.edit_message_media(media=types.InputMediaPhoto(open("../images/menu.jpg",'rb')),chat_id=message.chat.id,message_id=message.message_id)
+            self.bot.edit_message_caption("Купить Shick Объемом 110 мл, Цена - 2600 ₽",message.chat.id,message.message_id,reply_markup=markup)
+        def versal(message):#здесь
+            markup = types.InlineKeyboardMarkup(row_width=2)
+            button1 = types.InlineKeyboardButton("Купить",callback_data='versal 150')
+            markup.add(button1)
+            self.bot.edit_message_media(media=types.InputMediaPhoto(open("../images/menu.jpg",'rb')),chat_id=message.chat.id,message_id=message.message_id)
+            self.bot.edit_message_caption("Купить Versal Объемом 150 мл, Цена - ? ₽",message.chat.id,message.message_id,reply_markup=markup)
+        def lounge(message):#здесь
+            markup = types.InlineKeyboardMarkup(row_width=2)
+            button1 = types.InlineKeyboardButton("Купить",callback_data='lounge 50')
+            markup.add(button1)
+            self.bot.edit_message_media(media=types.InputMediaPhoto(open("../images/menu.jpg",'rb')),chat_id=message.chat.id,message_id=message.message_id)
+            self.bot.edit_message_caption("Купить Lounge Объемом 50 мл, Цена - 450 ₽",message.chat.id,message.message_id,reply_markup=markup)
+        def retro(message):#здесь
+            markup = types.InlineKeyboardMarkup(row_width=2)
+            button1 = types.InlineKeyboardButton("Купить",callback_data='retro 160')
+            markup.add(button1)
+            self.bot.edit_message_media(media=types.InputMediaPhoto(open("../images/menu.jpg",'rb')),chat_id=message.chat.id,message_id=message.message_id)
+            self.bot.edit_message_caption("Купить Retro Объемом 160 мл, Цена - 3600 ₽",message.chat.id,message.message_id,reply_markup=markup)
+
         @self.bot.message_handler(commands=["menu"])
         def menu(message):
             markup = types.InlineKeyboardMarkup(row_width=2)
-            button1 = types.InlineKeyboardButton("вау",callback_data='menu')
-            button2 = types.InlineKeyboardButton("авыаы",callback_data='profile')
-            button3 = types.InlineKeyboardButton("авы",callback_data='faq')
-            button4 = types.InlineKeyboardButton("отзаовы",callback_data='feedback')
-            button5 = types.InlineKeyboardButton("гарантии",callback_data='warranty')
-            button6 = types.InlineKeyboardButton("подержка",callback_data='support')
-            markup.add(button1,button2,button3,button4,button5,button6)
+            button1 = types.InlineKeyboardButton("Коллекция «Estetik” ",callback_data='estetik')
+            button2 = types.InlineKeyboardButton("Коллекция «Retro»",callback_data='retro')
+            markup.add(button1,button2)
             self.bot.edit_message_media(media=types.InputMediaPhoto(open("../images/menu.jpg",'rb')),chat_id=message.chat.id,message_id=message.message_id)
-            self.bot.edit_message_caption("Меню магазина",message.chat.id,message.message_id,reply_markup=markup)
+            self.bot.edit_message_caption("Категории",message.chat.id,message.message_id,reply_markup=markup)
         def profile(message):
             markup = types.InlineKeyboardMarkup(row_width=2)
             button1 = types.InlineKeyboardButton("история покупок",callback_data='menu')
@@ -48,7 +91,12 @@ class Bot:
         @self.bot.message_handler(commands=["faq"])
         def faq(message):   
             self.bot.send_message(message.chat.id, "Ответы на часто задаваемые вопросы!")
-            
+        def cart(message):
+            markup = types.InlineKeyboardMarkup(row_width=2)
+            button1 = types.InlineKeyboardButton("посмотреть корзину",callback_data='check cart')
+            button2 = types.InlineKeyboardButton("купить",callback_data='buy cart')
+            markup.add(button1,button2)
+            self.bot.edit_message_caption("Личный кабинет",message.chat.id,message.message_id,reply_markup=markup)
         @self.bot.callback_query_handler(func=lambda callback: True)
         def callback_message(callback):
             if callback.data=='menu':
@@ -68,4 +116,21 @@ class Bot:
             elif callback.data=='support':
                 self.bot.send_message(callback.message.chat.id,"Вы попали в подержку")
                 support(callback.message)
-    
+            elif callback.data == 'estetik 180':
+                self.cart.append((callback.message, callback.data,price_list[callback.data]))
+            elif callback.data == 'retro 160':
+                self.cart.append((callback.message, callback.data,price_list[callback.data]))
+            elif callback.data == 'bionika 160':
+                self.cart.append((callback.message, callback.data,price_list[callback.data]))
+            elif callback.data == 'versal 150':
+                self.cart.append((callback.message, callback.data,price_list[callback.data]))
+            elif callback.data == 'lounge 50':
+                self.cart.append((callback.message, callback.data,price_list[callback.data]))
+            elif callback.data == 'shikc 110':
+                self.cart.append((callback.message, callback.data,price_list[callback.data]))
+            elif callback.data == 'buy cart':
+                self.buy()
+    def buy(self):
+        reciept = "Ваш чек:\n"
+        for i in range(self.cart):
+            reciept+=f"{i+1}. "
